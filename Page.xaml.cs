4048b1670e7c5924554bb8b95cfedc74b1f47ce9
Nikilesh05 <121333004+Nikilesh05@users.noreply.github.com>
2023-01-19 17:46:53 +0530

    Add files via upload

---
+++ Page.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace file_Project
+{
+    /// <summary>
+    /// Interaction logic for Page.xaml
+    /// </summary>
+    public partial class Page
+    {
+        public Page()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        
+            File.WriteAllText("A:\\"+txtName.Text+".txt", txtpara.Text);
+        }
+    }
+}
